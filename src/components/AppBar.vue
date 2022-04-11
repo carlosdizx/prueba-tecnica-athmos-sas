29dbe3a88f316e181dedd7ea8793122ae7f7112b
Carlos <carlodiaz@umariana.edu.co>
2022-04-11 12:17:45 -0500

    Funcionando la coloración correctamente

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,14 +1,36 @@
 <template>
   <v-card color="white">
     <v-app-bar absolute>
-      <v-btn icon>
+      <v-btn text>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
-      <v-icon>mdi-home</v-icon>
+      <v-btn text>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
       <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab v-for="(tab, index) of tabs" :key="index">
+        <v-tabs align-with-title color="teal accent-3">
+          <v-tab
+            v-for="(tab, index) of tabs"
+            :key="index"
+            @click="item = index + 1"
+          >
+            <v-chip
+              v-if="item === index + 1"
+              dark
+              color="teal accent-3"
+              class="ma-2"
+            >
+              {{ 1 + index }}
+            </v-chip>
+            <v-chip
+              v-if="item !== index + 1"
+              dark
+              color="grey lighten-2"
+              class="ma-2"
+            >
+              {{ 1 + index }}
+            </v-chip>
             {{ tab.title }}
           </v-tab>
         </v-tabs>
@@ -23,6 +45,7 @@ import Vue from "vue";
 export default Vue.extend({
   name: "AppBar",
   data: () => ({
+    item: 1,
     tabs: [
       { title: "Tab 1" },
       { title: "Tab 2" },

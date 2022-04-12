c7029edef9a89a319cf5607690be452cf402ceac
Carlos <carlodiaz@umariana.edu.co>
2022-04-12 09:55:55 -0500

    Creada tabla que permitira gestionar las tareas

--- src/components/ListadoTareas.vue
+++ src/components/ListadoTareas.vue
@@ -1,14 +1,30 @@
 <template>
   <v-card>
-    <v-card-title><strong>Pasos inmediatos</strong></v-card-title>
-    <v-card-text>
-      Aqui van los pasos que completaste
-      <CrearTarea @registro="listar" />
-    </v-card-text>
-    <v-divider />
-    <v-card-text>
-      <h1>... Aquí van las tareas</h1>
-    </v-card-text>
+    <v-data-table
+      v-model="seleccionados"
+      :headers="columnas"
+      :items="filas"
+      :single-select="singleSelect"
+      item-key="name"
+      show-select
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-card-text>
+          <strong>Pasos inmediatos</strong>
+          <br />
+          Aqui van los pasos que completaste
+          <CrearTarea @registro="listar" />
+        </v-card-text>
+        <v-divider />
+        <v-switch
+          v-model="singleSelect"
+          label="Selección simple"
+          class="mx-2 my-1"
+        />
+      </template>
+    </v-data-table>
+    {{ seleccionados }}
   </v-card>
 </template>
 
@@ -19,10 +35,39 @@ import Vue from "vue";
 export default Vue.extend({
   name: "ListadoTareas",
   components: { CrearTarea },
+  data: () => ({
+    singleSelect: false,
+    seleccionados: [],
+    columnas: [
+      { text: "Titulo", value: "titulo" },
+      { text: "Descripción", value: "descripcion" },
+      { text: "Tags", value: "tags" },
+      { text: "Acciones", value: "acciones" },
+    ],
+    filas: [{}],
+  }),
   methods: {
-    listar(): void {
-      console.log("Listado");
+    async listar(): Promise<void> {
+      this.filas = [];
+      this.filas.push({
+        titulo: "Titulo 1",
+        descripcion: "Descripcion 1",
+        tags: ["Tag 1", "Tag 2", "Tag 3"],
+      });
+      this.filas.push({
+        titulo: "Titulo 2",
+        descripcion: "Descripcion 2",
+        tags: ["Tag 1", "Tag 2", "Tag 3"],
+      });
+      this.filas.push({
+        titulo: "Titulo 3",
+        descripcion: "Descripción 3",
+        tags: ["Tag 1", "Tag 2", "Tag 3"],
+      });
     },
+  },
+  created() {
+    this.listar();
   },
 });
 </script>
